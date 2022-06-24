599bf128ccfa2cc700445c5142133141c415478f
johnaslani <aslani.john@gmail.com>
2022-06-23 18:04:26 -0700

    Update exercises

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,21 @@
+// Write Your JavaScript Code Here
+
+
+// Uses assignment operator(=) to assign a value
+var personName = "John";
+var pets = 4;
+var funFact = "Yoga instructor"
+
+
+// To re-assign a variable, use only the variable's name  
+personName = "Alex";
+pets = 2;  
+funFact = "Swimming instructor"
+
+// Logs "My name is Tonya"
+console.log("My name is " + personName);
+
+
+console.log("I have" + pets + "pet(s).")
+
+console.log("FUn fact is `funFact`.")
